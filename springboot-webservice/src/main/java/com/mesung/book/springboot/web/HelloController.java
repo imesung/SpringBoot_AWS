813575b690efd221ee2e83cd75fe51358920d914
myhs2002 <myhs2002@gmail.com>
2020-01-20 22:16:08 +0900

    Test Code Practice

---
+++ springboot-webservice/src/main/java/com/mesung/book/springboot/web/HelloController.java
@@ -0,0 +1,12 @@
+package com.mesung.book.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
