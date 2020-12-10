1d5ab53ad7cea9a57c4c73c8c94be573ac619f12
mesung <myhs2002@gmail.com>
2020-12-10 23:23:24 +0900

    전체 글 조회하기

--- springboot-webservice/src/main/java/com/mesung/book/springboot/service/posts/PostsService.java
+++ springboot-webservice/src/main/java/com/mesung/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.mesung.book.springboot.service.posts;
 
 import com.mesung.book.springboot.domain.posts.Posts;
 import com.mesung.book.springboot.domain.posts.PostsRepository;
+import com.mesung.book.springboot.web.dto.PostsListResponseDto;
 import com.mesung.book.springboot.web.dto.PostsResponseDto;
 import com.mesung.book.springboot.web.dto.PostsSaveRequestDto;
 import com.mesung.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,10 +40,18 @@ public class PostsService {
     //이 상태에서 해당 데이터의 값을 변경하면 트랜잭션이 끝나는 시점에 해당 테이블에 변경분을 반영한다. 즉, Enttity 객체의 값만 변경하면
     //별도로 Update 쿼리를 날리 필요가 없다.(더티 체킹)
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
